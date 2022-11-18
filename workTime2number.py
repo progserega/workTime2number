ad829a94e75459ae3ec9ee18abd768f7929bb319
progserega <progserega@gmail.com>
2022-11-18 23:58:41 +1000

    запись в файл

--- workTime2number.py
+++ workTime2number.py
@@ -7,12 +7,19 @@ import os
 import csv
 
 if len(sys.argv)<2:
-  print("нужен один параметр - имя входного файла csv. Выход будет на экран")
+  print("нужен один параметр - имя входного файла csv. Выход будет в файл с постфиксом _converted.csv и на экран")
   sys.exit(1)
 with open(sys.argv[1], newline='\n') as csvfile:
   data = csv.DictReader(csvfile, delimiter=';')
+
+  # создаём файл:
+  out_file = open("%s_converted.csv"%sys.argv[1],"w+")
+  print("Дата;Время работы")
+  out_file.write("Дата;Время работы\n")
   for row in data:
     # переводим время в дробноче число часа:
     t = row['spent'].split(':')
     hours = float(int(t[0])*60+int(t[1]))/60
     print("%s;%f"%(row['day'],hours))
+    out_file.write("%s;%f\n"%(row['day'],hours))
+  out_file.close()
